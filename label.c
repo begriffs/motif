67f82c2bd47855518f507274ed7c8d13fa716b35
Joe Nelson <joe@begriffs.com>
2022-04-05 22:38:26 -0500

    Helpful note from manpage

--- label.c
+++ label.c
@@ -12,6 +12,15 @@ int main(int argc, char **argv)
 	int ac = 0;
 
 	toplevel = XtAppInitialize(&context, "", NULL, 0, &argc, argv, NULL, NULL, 0);
+
+	/* Yeppp, got burned by this initially:
+	 *
+	 * XtSetArg(3)
+	 *
+	 * Note that you should not use auto-increment or auto-decrement within the
+	 * first argument to XtSetArg.  XtSetArg can be implemented as a macro that
+	 * dereferences the first argument twice.
+	 */
 	XtSetArg(al[ac], XmNlabelString,
 			XmStringCreateLtoR("Hello world", char_set));
 	ac++;
